7d60781ab810a94da794f8c925ab53c338a39bc1
Polyspyder <160246422+Polyspyder@users.noreply.github.com>
2025-02-24 16:44:02 +0800

    walk animation added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,16 +4,28 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal")*speed, body.linearVelocity.y);
+        float HoriInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(HoriInput*speed, body.linearVelocity.y);
         
+        if(HoriInput > 0.01f)
+            transform.localScale = new Vector3(5,5,1);
+        else if ((HoriInput < -0.01f))
+            transform.localScale = new Vector3(-5,5,1);
+    
         if(Input.GetKey(KeyCode.Space))
             body.linearVelocity = new Vector2(body.linearVelocity.x, speed/2);
+
+
+        anim.SetBool("Walk", HoriInput != 0);
     }
 }
