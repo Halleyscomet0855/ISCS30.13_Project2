bf2e676671c5298b9ffa0a135190bd42cd25be02
Polyspyder <160246422+Polyspyder@users.noreply.github.com>
2025-02-24 21:58:42 +0800

    Jumping animations fixed. walljumps somehow added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ public class Movement : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D body;
     private Animator anim;
+    private bool Grounded;
 
     private void Awake()
     {
@@ -22,10 +23,22 @@ public class Movement : MonoBehaviour
         else if ((HoriInput < -0.01f))
             transform.localScale = new Vector3(-5,5,1);
     
-        if(Input.GetKey(KeyCode.Space))
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed/2);
+        if(Input.GetKey(KeyCode.Space) && Grounded)
+            Jump();
 
 
         anim.SetBool("Walk", HoriInput != 0);
+        anim.SetBool("Grounded", Grounded);
+    }
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        Grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Ground")
+            Grounded = true;
     }
 }
