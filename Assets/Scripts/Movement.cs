48a76ab7e56b07187d441f28c8c7e1a222e0ae0a
Halleyscomet08 <hunterxhalley@gmail.com>
2025-02-25 08:51:36 +0800

    implemented double-jump

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,11 +6,13 @@ public class Movement : MonoBehaviour
     private Rigidbody2D body;
     private Animator anim;
     private bool Grounded;
+    private int Jumps;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        Jumps = 2;
     }
 
     private void Update()
@@ -22,11 +24,19 @@ public class Movement : MonoBehaviour
             transform.localScale = new Vector3(5,5,1);
         else if ((HoriInput < -0.01f))
             transform.localScale = new Vector3(-5,5,1);
+        
+        if(Grounded && Jumps<2) {
+            Jumps = 2;
+        }
     
-        if(Input.GetKey(KeyCode.Space) && Grounded)
+        if(Input.GetKeyDown(KeyCode.Space) && Jumps > 0) {
             Jump();
+               
+            Debug.Log(Jumps);
+        }
+           
 
-
+        Debug.Log(Jumps);
         anim.SetBool("Walk", HoriInput != 0);
         anim.SetBool("Grounded", Grounded);
     }
@@ -34,6 +44,7 @@ public class Movement : MonoBehaviour
     {
         body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
         Grounded = false;
+        Jumps = Jumps - 1;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
